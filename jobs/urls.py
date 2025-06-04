903b073bab704d0bb4d77d66676a4055a99ce10f
tushar-gleecus <tushar.canchi@gleecus.com>
2025-06-04 15:16:35 +0530

    added buttons

--- jobs/urls.py
+++ jobs/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.job_list, name='job_list'),
+    path('create/', views.job_create, name='job_create'),
     path('<int:pk>/', views.job_detail, name='job_detail'),
+    path('<int:pk>/edit/', views.job_edit, name='job_edit'),
+    path('<int:pk>/delete/', views.job_delete, name='job_delete'),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,7 +1,8 @@
 # jobs/views.py
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Job
+from .forms import JobForm
 
 def job_list(request):
     jobs = Job.objects.all()
@@ -10,3 +11,30 @@ def job_list(request):
 def job_detail(request, pk):
     job = get_object_or_404(Job, pk=pk)
     return render(request, 'jobs/job_detail.html', {'job': job})
+
+def job_edit(request, pk):
+    job = get_object_or_404(Job, pk=pk)
+    form = JobForm(request.POST or None, instance=job)
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('job_detail', pk=job.pk)
+    return render(request, 'jobs/job_form.html', {'form': form})
+
+def job_delete(request, pk):
+    job = get_object_or_404(Job, pk=pk)
+    if request.method == 'POST':
+        job.delete()
+        return redirect('job_list')
+    return render(request, 'jobs/job_confirm_delete.html', {'job': job})
+
+def job_create(request):
+    if request.method == 'POST':
+        form = JobForm(request.POST)
+        if form.is_valid():
+            job = form.save(commit=False)
+            job.posted_by = request.user  # Or use a default user if not using auth
+            job.save()
+            return redirect('job_list')
+    else:
+        form = JobForm()
+    return render(request, 'jobs/job_form.html', {'form': form})
